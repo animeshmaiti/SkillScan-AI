96b20fdf68b6f3ed6b5718a4705eeed88c99b56f
animeshmaiti <a.maiti770@gmail.com>
2025-07-23 22:49:17 +0530

    init landing page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import ResumeCard from "~/components/ResumeCard";
+import { useState } from "react";
+import { resumes } from "../../constants";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "SkillScan AI" },
+    { name: "description", content: "Know your skills according to your resume and job requirements" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  // const [resumes,setResumes]=useState<Resume[]>([]);
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application and Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+      {
+        resumes.length > 0 && (
+          <div className="resumes-section">
+            {
+              resumes.map((resume) => (
+                <ResumeCard key={resume.id} resume={resume} />
+              ))
+            }
+          </div>
+        )
+      }
+    </section>
+  </main>;
 }
